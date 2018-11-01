41d2218cf34d9d72ff8cc08ba193a1f97d4daca4
AndyL <missedmemo@gmail.com>
2018-11-01 14:55:11 -0700

    render multiple components with legacy React syntax

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
+const Pet = () =>
+  React.createElement("div", {}, [
+    React.createElement("h2", {}, "Fifi"),
+    React.createElement("h2", {}, "dog"),
+    React.createElement("h2", {}, "Schnauzer")
+  ]);
+
 const App = () =>
-  React.createElement("div", {}, React.createElement("h1", {}, "Hey, React!"));
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adoptable..."),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
+  ]);
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
