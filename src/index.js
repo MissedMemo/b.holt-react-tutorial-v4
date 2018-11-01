8cc315ab5a092ed41fc58dc69776be018f876731
AndyL <missedmemo@gmail.com>
2018-11-01 15:05:54 -0700

    pass props to component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-const Pet = () =>
-  React.createElement("div", {}, [
-    React.createElement("h2", {}, "Fifi"),
-    React.createElement("h2", {}, "dog"),
-    React.createElement("h2", {}, "Schnauzer")
+const Pet = props => {
+  const { name, animal, breed } = props;
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
+};
 
 const App = () =>
   React.createElement("div", {}, [
     React.createElement("h1", {}, "Adoptable..."),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Fido",
+      animal: "dog",
+      breed: "mutt"
+    }),
+    React.createElement(Pet, {
+      name: "Saunders",
+      animal: "cat",
+      breed: "siamese"
+    }),
+    React.createElement(Pet, {
+      name: "Fritz",
+      animal: "fish",
+      breed: "goldfish"
+    })
   ]);
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
