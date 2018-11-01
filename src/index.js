f821324a3096f5136b403e0cf1b5a286c6003c79
AndyL <missedmemo@gmail.com>
2018-11-01 16:29:15 -0700

    convert functional to class component

--- src/index.js
+++ src/index.js
@@ -7,24 +7,35 @@ const Pet = props => {
   ]);
 };
 
-const App = () =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adoptable..."),
-    React.createElement(Pet, {
-      name: "Fido",
-      animal: "dog",
-      breed: "mutt"
-    }),
-    React.createElement(Pet, {
-      name: "Saunders",
-      animal: "cat",
-      breed: "siamese"
-    }),
-    React.createElement(Pet, {
-      name: "Fritz",
-      animal: "fish",
-      breed: "goldfish"
-    })
-  ]);
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked it!");
+  }
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adoptable Pets..."
+      ),
+      React.createElement(Pet, {
+        name: "Fido",
+        animal: "dog",
+        breed: "mutt"
+      }),
+      React.createElement(Pet, {
+        name: "Saunders",
+        animal: "cat",
+        breed: "siamese"
+      }),
+      React.createElement(Pet, {
+        name: "Fritz",
+        animal: "fish",
+        breed: "goldfish"
+      })
+    ]);
+  }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
