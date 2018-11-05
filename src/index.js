61a6b6f221d60d37d1d8814ade2d9a81c5016157
AndyL <missedmemo@gmail.com>
2018-11-05 09:08:16 -0800

    fix eslint plugin reference errors

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import { render } from "react-dom";
 import Pet from "./Pet.jsx";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked it!");
-  }
-
   render() {
     return (
       <Fragment>
-        <h1 onClick={this.handleTitleClick}>Adoptible Pets...</h1>
+        <h1>Adoptible Pets...</h1>
         <Pet name="Fido" animal="dog" breed="mutt" />
         <Pet name="Saunders" animal="cat" breed="siamese" />
         <Pet name="Fritz" animal="fish" breed="goldfish" />
