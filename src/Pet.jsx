3b529dd8b0cb006f887f21df113193285095ca62
AndyL <missedmemo@gmail.com>
2018-11-06 09:55:11 -0800

    convert Pet to class, to support state

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,11 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 
-const Pet = ({ name, animal, breed }) => (
-  <Fragment>
-    <h1>{name}</h1>
-    <h2>{animal}</h2>
-    <h2>{breed}</h2>
-  </Fragment>
-);
+class Pet extends Component {
+  render() {
+    const { name, animal, breed } = this.props;
+    return (
+      <Fragment>
+        <h1>{name}</h1>
+        <h2>{animal}</h2>
+        <h2>{breed}</h2>
+      </Fragment>
+    );
+  }
+}
 
 export default Pet;
