83bc6cca81d79940a419bca7f9b04afd06f3a16c
AndyL <missedmemo@gmail.com>
2018-11-07 14:18:12 -0800

    add reach router support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React, { Fragment } from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import Results from "./results.jsx";
+import Details from "./details.jsx";
 
 class App extends React.Component {
   render() {
     return (
       <Fragment>
         <h1>Adoptible Pets...</h1>
-        <Results />
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </Fragment>
     );
   }
