e64261025d8c5a261fa6dc7498fdf04c27d7b28b
AndyL <missedmemo@gmail.com>
2018-11-06 16:22:10 -0800

    move content out of App container

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
 import React, { Fragment } from "react";
 import { render } from "react-dom";
-import Pet from "./Pet.jsx";
-import petsAPI from "petfinder-client";
-
-const petFinder = petsAPI({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-const resolvePets = pets =>
-  pets & pets.pet || Array.isArray(pets.pet) ? pets.pet : [pets.pet];
-
-const resolveBreed = breed => (Array.isArray(breed) ? breed.join(", ") : breed);
+import Results from "./results.jsx";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    petFinder.pet
-      .find({ output: "full", location: "San Francisco, CA" })
-      .then(data => {
-        const pets = resolvePets(data.petfinder.pets);
-        if (pets) {
-          this.setState({ pets });
-        }
-      });
-  }
-
   render() {
     return (
       <Fragment>
         <h1>Adoptible Pets...</h1>
-        <div>
-          {this.state.pets.map(pet => (
-            <Pet
-              key={pet.id}
-              name={pet.name}
-              animal={pet.animal}
-              breed={resolveBreed(pet.breeds.breed)}
-              media={pet.media}
-              location={`${pet.contact.city}, ${pet.contact.state}`}
-            />
-          ))}
-        </div>
+        <Results />
       </Fragment>
     );
   }
---
+++ src/results.jsx
@@ -0,0 +1,52 @@
+import React, { Component, Fragment } from "react";
+import Pet from "./pet.jsx";
+import petsAPI from "petfinder-client";
+
+const petFinder = petsAPI({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+const resolvePets = pets =>
+  pets & pets.pet || Array.isArray(pets.pet) ? pets.pet : [pets.pet];
+
+const resolveBreed = breed => (Array.isArray(breed) ? breed.join(", ") : breed);
+
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petFinder.pet
+      .find({ output: "full", location: "San Francisco, CA" })
+      .then(data => {
+        const pets = resolvePets(data.petfinder.pets);
+        if (pets) {
+          this.setState({ pets });
+        }
+      });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => (
+          <Pet
+            key={pet.id}
+            name={pet.name}
+            animal={pet.animal}
+            breed={resolveBreed(pet.breeds.breed)}
+            media={pet.media}
+            location={`${pet.contact.city}, ${pet.contact.state}`}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Results;
