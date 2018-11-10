7cb0f3216046a05c184aeb46e0a1ec740d75c0ba
AndyL <missedmemo@gmail.com>
2018-11-09 20:42:50 -0800

    add animal drop list

--- src/search-params.jsx
+++ src/search-params.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ANIMALS } from "petfinder-client";
 
 class SearchParams extends Component {
   state = {
@@ -12,17 +13,38 @@ class SearchParams extends Component {
     this.setState({ location });
   };
 
+  handleAnimalChange = e => {
+    const animal = e.target.value;
+    this.setState({ animal });
+  };
+
   render() {
     return (
       <div className="search-params">
         <label htmlFor="location">
           Location:
           <input
-            onChange={this.handleLocationChange}
             id="location"
+            onChange={this.handleLocationChange}
             value={this.state.location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal:
+          <select
+            id="animal"
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+            value={this.state.animal}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </div>
     );
