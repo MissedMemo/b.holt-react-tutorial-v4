75fb98eda07700fe49ffb08b6bd61d919062bf95
AndyL <missedmemo@gmail.com>
2018-11-12 22:01:04 -0800

    handle submit on stand-alone search-params page

--- src/search-params.jsx
+++ src/search-params.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import SearchBox from "./search-box.jsx";
+import { navigate } from "@reach/router";
 
 class SearchParams extends Component {
+  handleSubmit() {
+    navigate("/");
+  }
+
   render() {
     return (
       <div className="search-route">
-        <SearchBox />
+        <SearchBox search={this.handleSubmit} />
       </div>
     );
   }
